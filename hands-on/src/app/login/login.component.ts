295cceabdee840ec33eef8a8b9dd11da5f211c8f
NiteeshRavichandran <niteeshravichandran@gmail.com>
2023-09-13 16:35:03 +0530

    hands on

---
+++ hands-on/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(null),
+      password: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
+}
